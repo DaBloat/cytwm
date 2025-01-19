7b9e23c603a5f50c59498495eaae07f2af82de81
Kurt Russel Villamor <134671520+DaBloat@users.noreply.github.com>
2025-01-19 13:46:11 +0800

    Updated Theme updater
    - Hyprctl reloads the default conf every time reboot
    - Drop hyprctl commands and the script is focused in more editing the looks.conf

--- .scripts/mode_switcher.sh
+++ .scripts/mode_switcher.sh
@@ -1,22 +1,58 @@
-# This script is for switching to light or dark mode to whole system
+# This script is for switching to light or dark mode to Hyprland
 
 WALL_PATH=$HOME/wallpaper
 LCOLOR_BORDER=331e12
-DCOLOR_BORDER=d4baa4 
+DCOLOR_BORDER=d4baa4
+HYPR_CONF=$HOME/.config/hypr/config/aesthetics/looks.conf
 
-hyprctl reload
+# Hyprctl always defaults the color in the conf file every configuration in reload/reboot
+# Thus saving it to the looks.conf solve the problem
+add_to_default() {
+	local color="${1}"
+	local ACTIVE="    col.active_border = rgb(${color})"
+	local INACTIVE="    col.inactive_border = rgb(${color})"
+	local SHADOW_COLOR="        color = rgb(${color})"
+	local SHADOW_OFFSET="        offset = $2 $2"
+
+
+
+	# Active Border
+	if grep -q "col.active_border" "$HYPR_CONF"; then
+		sed -i "s/^.*col.active_border.*/$ACTIVE/" "$HYPR_CONF"
+	else
+		awk '/general {/ { print; print "'"$ACTIVE"'"; next }1' "$HYPR_CONF" > "$HYPR_CONF.tmp" && mv "$HYPR_CONF.tmp" "$HYPR_CONF"
+	fi
+	
+	# Inactive Border
+	if grep -q "col.inactive_border" "$HYPR_CONF"; then
+		sed -i "s/^.*col.inactive_border.*/$INACTIVE/" "$HYPR_CONF"
+	else
+		awk '/general {/ { print; print "'"$INACTIVE"'"; next }1' "$HYPR_CONF" > "$HYPR_CONF.tmp" && mv "$HYPR_CONF.tmp" "$HYPR_CONF"
+	fi
+
+	# Shadow Color
+	if grep -q "color" "$HYPR_CONF"; then
+		sed -i "s/^.*color.*/$SHADOW_COLOR/" "$HYPR_CONF"
+	else
+		awk '/shadow {/ { print; print "'"$SHADOW_COLOR"'"; next }1' "$HYPR_CONF" > "$HYPR_CONF.tmp" && mv "$HYPR_CONF.tmp" "$HYPR_CONF"
+	fi
+
+	# Offset
+	if grep -q "offset" "$HYPR_CONF"; then
+		sed -i "s/^.*offset.*/$SHADOW_OFFSET/" "$HYPR_CONF"
+	else
+		awk '/shadow {/ { print; print "'"$SHADOW_OFFSET"'"; next }1' "$HYPR_CONF" > "$HYPR_CONF.tmp" && mv "$HYPR_CONF.tmp" "$HYPR_CONF"
+	fi
+
+
+}
 
 if [[ $1 == 'l' ]] then
-	echo 'Light MODE ON'
 	swww img $WALL_PATH/wall.png
-	hyprctl --batch "keyword general:col.active_border 0xFF${LCOLOR_BORDER} ;
-			 keyword general:col.inactive_border 0xFF${LCOLOR_BORDER} ;
-			 keyword decoration:shadow:color 0xFF${LCOLOR_BORDER};
-			 keyword decoration:shadow:offset 4 4 ;
-			 keyword decoration:shadow:scale 1.0"
+	add_to_default $LCOLOR_BORDER 4
+
 elif [[ $1 == 'd' ]] then
-	echo 'Dark MODE ON'
-	$(swww img $WALL_PATH/wall-dark.png)
-	hyprctl --batch "keyword general:col.active_border 0xFF${DCOLOR_BORDER} ; keyword general:col.inactive_border 0xFF${DCOLOR_BORDER}"
+	swww img $WALL_PATH/wall-dark.png
+	add_to_default $DCOLOR_BORDER 2
 fi
 
