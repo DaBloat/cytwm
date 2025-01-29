ce2651d2c64290ab1e3bdc47db9e0f509874090f
Kurt Russel Villamor <134671520+DaBloat@users.noreply.github.com>
2025-01-29 23:56:02 +0800

    Per window aScreenshot

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -2,5 +2,10 @@
 
 SCREEN_SHOT=$HOME/Pictures/screenshots/$(date +'%s.png')
 
-grim -g "$(slurp -w 0)" $SCREEN_SHOT
+POS=$(hyprctl activewindow | grep "at:" | awk -F ': ' '{print $2}')
+SIZE=$(hyprctl activewindow | grep "size:" | awk -F ': ' '{print $2}' | awk -F ',' '{print $1 "x" $2}')
+
+echo "$(slurp -w 0)"
+echo "$POS $SIZE"
+grim -g "$POS $SIZE" $SCREEN_SHOT
 wl-copy --type image/png < "$SCREEN_SHOT"
