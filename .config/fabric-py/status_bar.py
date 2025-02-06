7d7de6efcb66bc2c7046ae003fc30d5a16a66f11
Kurt Russel <134671520+DaBloat@users.noreply.github.com>
2025-02-07 07:27:35 +0800

    updates to initial design of the workspace

--- .config/fabric-py/status_bar.py
+++ .config/fabric-py/status_bar.py
@@ -24,9 +24,9 @@ class StatusBar(Window):
 
         self.workspace = Workspaces(
             name = 'workspaces',
-            spacing= 10,
+            spacing= 2,
             buttons = [WorkspaceButton(id=wd+1, label=f'{wd+1}') for wd in range(5)],
-            buttons_factory= lambda wd: WorkspaceButton(id=wd, label=f'{wd}') if  wd != -99 else WorkspaceButton(id=wd, label='') 
+            buttons_factory= lambda wd: WorkspaceButton(id=wd, label=f"{wd}") if  wd != -99 else WorkspaceButton(id=wd, label="\uf21b") 
         )
         print(self.workspace._active_workspace)
 
