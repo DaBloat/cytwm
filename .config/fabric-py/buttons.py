6070dca0d030ef0bde3da025234761c161a2c3be
Kurt Russel <134671520+DaBloat@users.noreply.github.com>
2025-02-11 22:17:26 +0800

    Added Battery Buttons, Need to more about the python

---
+++ .config/fabric-py/buttons.py
@@ -0,0 +1,57 @@
+import subprocess
+from fabric.widgets.button import Button
+from fabric.widgets.box import Box
+from fabric.widgets.label import Label
+from datetime import datetime
+from fabric.widgets.image import Image
+from fabric.utils import invoke_repeater
+
+class ClockButton(Button):
+    def __init__(self, widget=None):
+        super().__init__(
+            name='clock-button',
+            on_clicked = lambda *_ : widget.show() if not widget.is_visible() else widget.hide() 
+        )
+        self.time = Label()
+        self.children = self.time
+        
+        invoke_repeater(1000, self.update_time)
+        
+        
+    def update_time(self):
+        self.time.set_label(f'{datetime.now().strftime('%I:%M %p')}')
+        return True
+    
+class BatteryButton(Button):
+    def __init__(self, widget=None):
+        super().__init__(
+            name='battery-button',
+            on_clicked = lambda *_ : widget.show() if not widget.is_visible() else widget.hide() 
+        )
+        self.battery_icons = {'Discharging':Image('icons/directory/battery.svg'), 'Charging':Image('icons/directory/battery_charging.svg')}
+        self.battery_percent = Label()
+        
+        self.children = [self.battery_percent]
+        invoke_repeater(1000, self.display_battery)
+        
+    def fetch_status(self):
+        status = subprocess.run("cat /sys/class/power_supply/BAT1/status",
+                                 capture_output=True,
+                                 text=True, 
+                                 shell=True).stdout.strip()
+        return status
+    
+    def fetch_percentage(self):
+        percent = subprocess.run("cat /sys/class/power_supply/BAT1/capacity",
+                                 capture_output=True,
+                                 text=True, 
+                                 shell=True).stdout.strip()
+        return percent
+        
+    def display_battery(self):
+        self.battery_percent.set_label(f'{self.fetch_percentage()}%')
+        return True
+
+        
+        
+        
