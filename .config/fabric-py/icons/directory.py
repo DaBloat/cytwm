45290ffaffae8eb2520ed6fd0a55bf064125a44c
Kurt Russel <134671520+DaBloat@users.noreply.github.com>
2025-02-15 20:19:35 +0800

    Experiment

--- .config/fabric-py/icons/directory.py
+++ .config/fabric-py/icons/directory.py
@@ -1,5 +1,6 @@
 from fabric.widgets.image import Image
+from fabric.utils import get_relative_path
 
 class Icons:
     def __init__(self):
-        pass
+        self.menu = Image(get_relative_path('directory/menu.svg'))
