d7524380066379eeb7f82a7f500e006868224d9e
Kurt Russel <134671520+DaBloat@users.noreply.github.com>
2025-03-05 19:26:03 +0800

    FABRIC-PY IS NOW THE FABRIC BLOCK

--- .config/fabric-py/components/misc.py
+++
@@ -1,9 +0,0 @@
-from fabric.widgets.label import Label
-
-
-class Separator(Label):
-    def __init__(self):
-        super().__init__(
-            '|',
-            name = 'separators',
-        )
--- .config/fabric-py/icons/directory.py
+++
@@ -1,6 +0,0 @@
-from fabric.widgets.image import Image
-from fabric.utils import get_relative_path
-
-class Icons:
-    def __init__(self):
-        self.menu = Image(get_relative_path('directory/menu.svg'))
