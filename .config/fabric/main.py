c647b288d6cdb48277971995b92360430c030042
Kurt Russel <134671520+DaBloat@users.noreply.github.com>
2025-03-06 09:44:53 +0800

    Main.css updates

--- .config/fabric/main.py
+++ .config/fabric/main.py
@@ -4,15 +4,14 @@ from fabric.utils import monitor_file, invoke_repeater, get_relative_path
 
 def set_css(*_):
         return app.set_stylesheet_from_file(
-            get_relative_path("./main.css")
+            get_relative_path("main.css")
     )
 
 if __name__ == '__main__':
     bar = StatusBar()
-    app = Application('status', bar)
+    app = Application('status-bar', bar)
     
-    monitor = monitor_file(get_relative_path('./style'))
+    monitor = monitor_file(get_relative_path('style/cyna-colors.css'))
     monitor.connect('changed', set_css)
     set_css()
     app.run()
-
