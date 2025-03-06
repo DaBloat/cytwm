d92f9c87ac1f553e6722cfabedb1b3d496f4938a
Kurt Russel <134671520+DaBloat@users.noreply.github.com>
2025-03-06 22:47:08 +0800

    SPECIAL WORKSPACES TREATMENT

--- .config/fabric/status_bar/components/workspace.py
+++ .config/fabric/status_bar/components/workspace.py
@@ -1,11 +1,12 @@
 from fabric.hyprland.widgets import Workspaces, WorkspaceButton
 
+
 class WorkSpace(Workspaces):
     def __init__(self):
         super().__init__(
             name = 'workspaces',
             spacing = 5,
             buttons = [WorkspaceButton(id=wd+1, label=None) for wd in range(5)],
-            buttons_factory = lambda wd: WorkspaceButton(id=wd, label=None) if wd != -99 else WorkspaceButton(id=wd, label="SECRET")
+            buttons_factory = lambda wd: WorkspaceButton(id=wd, label=None) if wd != -99 else None
         )
 
