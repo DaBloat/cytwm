953f69f86f0c33813e3434f5a6e5d7698cf30bf0
Kurt Russel <134671520+DaBloat@users.noreply.github.com>
2025-03-09 11:14:59 +0800

    ADDITION OF SEPERATORS AND POPUPS

--- .config/fabric/status_bar/components/profile.py
+++ .config/fabric/status_bar/components/profile.py
@@ -1,15 +1,21 @@
+from status_bar.components.misc import PopupWindow
 from fabric.widgets.button import Button
 from fabric.widgets.box import Box
 from fabric.widgets.label import Label
 
 class Profile(Button):
-    def __init__(self):
+    def __init__(self, widget=None):
         super().__init__(
-            name='profile'
+            name='profile',
+            on_clicked = lambda *_ : widget.show() if not widget.is_visible() else widget.hide() 
         )
         self.children = Label('')
         
-class ProfileWidgets:
-    def __init__(self):
-        pass
+class ProfileWidgets(PopupWindow):
+    def __init__(self, parent):
+        super().__init__(
+            parent=parent,
+            visible = False,
+            all_visible = False)
+        self.children = Label('Test')
     
