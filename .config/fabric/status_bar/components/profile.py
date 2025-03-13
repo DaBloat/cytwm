6ab89a924b9b021ca16185f9ed273a40913aaa76
Kurt Russel <134671520+DaBloat@users.noreply.github.com>
2025-03-13 09:52:13 +0800

    UPDATES AND TWEAKS

--- .config/fabric/status_bar/components/profile.py
+++ .config/fabric/status_bar/components/profile.py
@@ -3,9 +3,8 @@ from fabric.widgets.button import Button
 from fabric.widgets.box import Box
 from fabric.widgets.label import Label
 from status_bar.components.misc import RoundedImage
-from fabric.utils import get_relative_path
-import cairo
-import getpass
+from fabric.utils import get_relative_path, invoke_repeater
+import getpass, socket, os, psutil, time
 
 class Profile(Button):
     def __init__(self, widget=None):
@@ -36,21 +35,42 @@ class ProfileWidgets(PopupWindow):
                 children=[self.left_wing(), self.middle_wing(), self.right_wing()]
         )
         
+        invoke_repeater(1000, self.uptime_update)
+        
+        
+    def uptime_update(self):
+        uptime = str((time.time() - psutil.boot_time()) / 3600).split('.')
+        hour = int(uptime[0])
+        minute = int(float('.'+uptime[1]) * 60)
+        self.uptime.set_label(f'{hour} Hour and {minute} minute(s)')
+        return True
+        
+        
     def left_wing(self):
         self.whoami = Box(
             orientation='v',
             name='whoami',
             children=[self.pfp, 
                       Label(f'{str(getpass.getuser()).capitalize()}', name='user'),
-                      Label('DaBloat', style='font-size: 12px; ', name='alias')]    
-        )
+                      Label(f'{socket.gethostname()}', name='alias')]    
+        )       
+        
         return Box(
+            orientation='v',
             children=[self.whoami]
         )
     
     def middle_wing(self):
+        self.uptime = Label()
+        self.uptime_box = Box(
+            orientation='v',
+            name='uptime-box',
+            children=[self.uptime, Label('Uptime')]
+        )
+        
         return Box(
-            children=Label('middle')
+            v_align='start',
+            children=[self.uptime_box]
         )
     
     def right_wing(self):
